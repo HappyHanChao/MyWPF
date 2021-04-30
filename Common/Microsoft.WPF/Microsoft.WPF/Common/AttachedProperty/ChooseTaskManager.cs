3133370bcfdb0be4afa68688dea736af5105629a
chaorenbuhuifei7 <790949837@qq.com>
2021-04-30 15:36:54 +0800

    add

---
+++ Common/Microsoft.WPF/Microsoft.WPF/Common/AttachedProperty/ChooseTaskManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Microsoft.WPF.Common
+{
+    public class ChooseTaskManager: DependencyObject
+    {
+
+        public static int GetMainNodeID(DependencyObject obj)
+        {
+            return (int)obj.GetValue(MainNodeIDProperty);
+        }
+
+        public static void SetMainNodeID(DependencyObject obj, int value)
+        {
+            obj.SetValue(MainNodeIDProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MainNodeIDProperty =
+            DependencyProperty.RegisterAttached("MainNodeID", typeof(int), typeof(ChooseTaskManager), new PropertyMetadata(0,OnMainNodeIDChanged));
+
+        private static void OnMainNodeIDChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Button button = d as Button;
+
+        }
+    }
+}
---
+++ Common/Microsoft.WPF/Microsoft.WPF/Common/ControlsSearchHelper.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Microsoft.WPF.Common
+{
+    public class ControlsSearchHelper
+    {
+        /// <summary>
+        /// 查找父控件
+        /// </summary>
+        /// <typeparam name="T">父控件的类型</typeparam>
+        /// <param name="obj">要找的是obj的父控件</param>
+        /// <param name="name">想找的父控件的Name属性</param>
+        /// <returns>目标父控件</returns>
+        public static T GetParentObject<T>(DependencyObject obj, string name) where T : FrameworkElement
+        {
+            DependencyObject parent = VisualTreeHelper.GetParent(obj);
+
+            while (parent != null)
+            {
+                if (parent is T && (((T)parent).Name == name | string.IsNullOrEmpty(name)))
+                {
+                    return (T)parent;
+                }
+
+                // 在上一级父控件中没有找到指定名字的控件，就再往上一级找
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            return null;
+        }
+
+
+       
+        public static T GetChildObject<T>(DependencyObject obj, string name) where T : FrameworkElement
+        {
+            DependencyObject child = null;
+            T grandChild = null;
+
+            for (int i = 0; i <= VisualTreeHelper.GetChildrenCount(obj) - 1; i++)
+            {
+                child = VisualTreeHelper.GetChild(obj, i);
+
+                if (child is T && (((T)child).Name == name | string.IsNullOrEmpty(name)))
+                {
+                    return (T)child;
+                }
+                else
+                {
+                    // 在下一级中没有找到指定名字的子控件，就再往下一级找
+                    grandChild = GetChildObject<T>(child, name);
+                    if (grandChild != null)
+                        return grandChild;
+                }
+            }
+
+            return null;
+
+        }
+
+
+       
+        public static List<T> GetChildObjects<T>(DependencyObject obj, string name) where T : FrameworkElement
+        {
+            DependencyObject child = null;
+            List<T> childList = new List<T>();
+
+            for (int i = 0; i <= VisualTreeHelper.GetChildrenCount(obj) - 1; i++)
+            {
+                child = VisualTreeHelper.GetChild(obj, i);
+
+                if (child is T && (((T)child).Name == name || string.IsNullOrEmpty(name)))
+                {
+                    childList.Add((T)child);
+                }
+
+                childList.AddRange(GetChildObjects<T>(child, ""));
+            }
+
+            return childList;
+        }
+
+        public static List<T> GetBrotherObjects<T>(DependencyObject obj, string name) where T : FrameworkElement
+        {
+            DependencyObject child = null;
+            List<T> childList = new List<T>();
+            DependencyObject parent = VisualTreeHelper.GetParent(obj);
+
+
+            for (int i = 0; i <= VisualTreeHelper.GetChildrenCount(parent) - 1; i++)
+            {
+                child = VisualTreeHelper.GetChild(parent, i);
+
+                if (child is T && (((T)child).Name == name || string.IsNullOrEmpty(name)))
+                {
+                    childList.Add((T)child);
+                }
+
+                childList.AddRange(GetChildObjects<T>(child, ""));
+            }
+
+            return childList;
+
+        }
+    }
+}
